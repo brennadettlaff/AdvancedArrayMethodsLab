cc9092556c9c6be61b67257665e4759c9ec538a7
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-09-06 15:21:34 -0600

    completed problem nine

--- app.js
+++ app.js
@@ -259,19 +259,28 @@ console.log('mexicanFood from filterExample', mexicanFood)
 //8a. Create a function that will return an array of the string cuisine types. Ie, ["Italian", "Italian", "Mexican", ...]
 //Map
 
-function problemEight(){
+// function problemEight(){
 
-    let results;
-    results = dishes.map(function(element){return element.cuisine})
-    return results
-}
+//     let results;
+//     results = dishes.map(function(element){return element.cuisine})
+//     return results
+// }
 
-let cuisineTypes = problemEight()
-console.log("Cuisine Types", cuisineTypes)
+// let cuisineTypes = problemEight()
+// console.log("Cuisine Types", cuisineTypes)
 
 //9. Create a function that will return an array of strings, with the cuisine type appended to the start of the dish's name. Ie, ["Italian Pizza", "Italian Spaghetti", ...]
 //Map 
 
+function problemNine(){
+
+    let results;
+    results = dishes.map(function(el){return el.cuisine + " " + el.name})
+    return results
+}
+
+let cuisineTypeName = problemNine()
+console.log("Cuisine Type and Name", cuisineTypeName)
 
 
 //10. Create a function that will use advanced array methods on the 'dishes' array and return the result ["Vegetarian Lasagna", "Vegetarian Falafel", "Vegetarian Chili"]
